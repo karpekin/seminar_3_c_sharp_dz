7e982de2473a906820bd947c9a35161f964e8081
karpekin <karpekin@mail.ru>
2023-04-16 12:06:54 +0300

    task 19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное чило: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = number/10000;
+int b = number/1000%10;
+int c = number/10%10;
+int d = number%10;
+
+if (a == d && b == c)
+{  
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
